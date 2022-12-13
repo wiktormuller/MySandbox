8b392ab4350cf7541ad2534c06f640d14145a1d4
Wiktor Muller <biznes.wiktor@gmail.com>
2022-12-13 19:15:30 +0100

    Extended version of smarter enums

--- SmarterEnums/SmarterEnums/Subscriptions.cs
+++ SmarterEnums/SmarterEnums/Subscriptions.cs
@@ -2,17 +2,44 @@ using Ardalis.SmartEnum;
 
 namespace SmarterEnums;
 
-public class Subscriptions : SmartEnum<Subscriptions>
+public abstract class Subscriptions : SmartEnum<Subscriptions>
 {
-    public static readonly Subscriptions Free = new("Free", 1, .0);
-    public static readonly Subscriptions Premium = new("Premium", 2, .25);
-    public static readonly Subscriptions Vip = new("VIP", 3, .5);
+    public static readonly Subscriptions Free = new FreeTier();
+    public static readonly Subscriptions Premium = new PremiumTier();
+    public static readonly Subscriptions Vip = new VipTier();
     
     // We can extend the default constructor of (string name, int value) to (string name, int value, double discount)
-    public double Discount { get; }
+    // Or we can use inheritance to extend the Subscriptions that will be abstract
+    public abstract double Discount { get; }
 
-    public Subscriptions(string name, int value, double discount) : base(name, value)
+    private Subscriptions(string name, int value/*, double discount*/) : base(name, value)
     {
-        Discount = discount;
+    }
+    
+    private sealed class FreeTier : Subscriptions
+    {
+        public FreeTier() : base("Free", 1)
+        {
+        }
+
+        public override double Discount => .0;
+    }
+    
+    private sealed class PremiumTier : Subscriptions
+    {
+        public PremiumTier() : base("Premium", 2)
+        {
+        }
+
+        public override double Discount => .25;
+    }
+    
+    private sealed class VipTier : Subscriptions
+    {
+        public VipTier() : base("VIP", 3)
+        {
+        }
+
+        public override double Discount => .5;
     }
 }
