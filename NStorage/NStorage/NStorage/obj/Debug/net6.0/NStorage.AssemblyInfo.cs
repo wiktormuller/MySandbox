089fe690301080995faf6ccf803183236fe18d89
Wiktor Muller <biznes.wiktor@gmail.com>
2022-12-23 11:50:29 +0100

    NuGet description for NStorage project

--- NStorage/NStorage/NStorage/obj/Debug/net6.0/NStorage.AssemblyInfo.cs
+++ NStorage/NStorage/NStorage/obj/Debug/net6.0/NStorage.AssemblyInfo.cs
@@ -10,8 +10,9 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("NStorage")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Some author")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyDescriptionAttribute("NStorage - lorem impsum")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("NStorage")]
